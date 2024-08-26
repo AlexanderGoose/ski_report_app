e989d3e398dd2cc7c06a74589f766e47a0a74baf
AlexanderGoose <alexandergoose97@gmail.com>
2024-08-26 08:49:04 -0600

    added coordinates and pass info for major co resorts

---
+++ resort_data.py
@@ -0,0 +1,136 @@
+
+
+resorts = {
+
+    'Arapahoe Basin': {
+        'LON': 39.6341,
+        'LAT': -105.8715,
+        'PASS': 'Ikon'
+    },
+
+    'Aspen Highlands': {
+        'LON': 39.1820,
+        'LAT': -106.8564,
+        'PASS': 'Ikon'
+    },
+        
+    'Aspen Mountain': {
+        'LON': 39.1761,
+        'LAT': -106.8292,
+        'PASS': 'Ikon'
+    },
+
+    'Beaver Creek': {
+        'LON': 39.6017,
+        'LAT': -106.5316,
+        'PASS': 'Epic'
+    },
+
+    'Breckenridge': {
+        'LON': 39.4783,
+        'LAT': -106.0723,
+        'PASS': 'Epic'
+    },
+
+    'Buttermilk (Aspen)': {
+        'LON': 39.2058,
+        'LAT': -106.8611,
+        'PASS': 'Ikon'
+    },
+
+    'Copper Mountain': {
+        'LON': 39.4790,
+        'LAT': -106.1613,
+        'PASS': 'Ikon'
+    },
+
+    'Crested Butte': {
+        'LON': 38.9122,
+        'LAT': -106.9624,
+        'PASS': 'Both'
+    },
+
+    'Eldora': {
+        'LON': 39.9495,
+        'LAT': -105.5702,
+        'PASS': 'Ikon'
+    },
+
+    'Keystone': {
+        'LON': 39.5792,
+        'LAT': -105.9347,
+        'PASS': 'Epic'
+    },
+
+    'Loveland': {
+        'LON': 39.680,
+        'LAT': -105.897,
+        'PASS': None
+    },
+
+    'Monarch Mountain': {
+        'LON': 38.5121,
+        'LAT': -106.3320,
+        'PASS': None
+    },
+
+    'Powderhorn': {
+        'LON': 39.0694,
+        'LAT': -108.1507,
+        'PASS': None
+    },
+
+    'Purgatory': {
+        'LON': 37.6277,
+        'LAT': -107.8376,
+        'PASS': None
+    },
+
+    'Silverton': {
+        'LON': 37.8846,
+        'LAT': 107.6659,
+        'PASS': None
+    },
+
+    'Ski Cooper': {
+        'LON': 39.3602,
+        'LAT': -106.3014,
+        'PASS': None
+    },
+
+    'Snowmass (Aspen)': {
+        'LON': 39.1895,
+        'LAT': -106.9497,
+        'PASS': 'Ikon'
+    },
+
+    'Steamboat': {
+        'LON': 40.4850,
+        'LAT': -106.8317,
+        'PASS': 'Ikon'
+    },
+
+    'Telluride': {
+        'LON': 37.9375,
+        'LAT': -107.8123,
+        'PASS': 'Epic'
+    },
+
+    'Vail': {
+        'LON': 39.6061,
+        'LAT': -106.3550,
+        'PASS': 'Epic'
+    },
+
+    'Winter Park': {
+        'LON': 39.8869,
+        'LAT': -105.7625,
+        'PASS': 'Ikon'
+    },
+
+    'Wolf Creek': {
+        'LON': 37.4722,
+        'LAT': -106.7930,
+        'PASS': None
+    },
+}
