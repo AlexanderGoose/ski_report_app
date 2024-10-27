ddacf536aaf6708964c3c54634452c041993a7c7
AlexanderGoose <alexandergoose97@gmail.com>
2024-10-26 22:06:04 -0600

    readding ikon data with coords and names

---
+++ ikon.py
@@ -0,0 +1,61 @@
+resorts_lst = ['Arapahoe Basin', 'Aspen Highlands', 'Aspen Mountain',
+               'Buttermilk (Aspen)', 'Copper Mountain', 'Eldora',
+               'Snowmass (Aspen)', 'Steamboat', 'Winter Park']
+
+resorts = {
+
+    'Arapahoe Basin': {
+        'LAT': 39.6341,
+        'LON': -105.8715,
+        'PASS': 'Ikon'
+    },
+
+    'Aspen Highlands': {
+        'LAT': 39.1820,
+        'LON': -106.8564,
+        'PASS': 'Ikon'
+    },
+        
+    'Aspen Mountain': {
+        'LAT': 39.1761,
+        'LON': -106.8292,
+        'PASS': 'Ikon'
+    },
+
+    'Buttermilk (Aspen)': {
+        'LAT': 39.2058,
+        'LON': -106.8611,
+        'PASS': 'Ikon'
+    },
+
+    'Copper Mountain': {
+        'LAT': 39.4790,
+        'LON': -106.1613,
+        'PASS': 'Ikon'
+    },
+
+    'Eldora': {
+        'LAT': 39.9495,
+        'LON': -105.5702,
+        'PASS': 'Ikon'
+    },
+
+    'Snowmass (Aspen)': {
+        'LAT': 39.1895,
+        'LON': -106.9497,
+        'PASS': 'Ikon'
+    },
+
+    'Steamboat': {
+        'LAT': 40.4850,
+        'LON': -106.8317,
+        'PASS': 'Ikon'
+    },
+
+    'Winter Park': {
+        'LAT': 39.8869,
+        'LON': -105.7625,
+        'PASS': 'Ikon'
+    },
+
+}
